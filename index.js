3a264e2008812a719477458e66bba806b6f0bc82
MoritzGitH <143597441+MoritzGitH@users.noreply.github.com>
2023-11-22 09:51:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://codecamp-70d45-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const cartInDB =ref(database, "cart")
+
+const inputFieldEl = document.getElementById('input-field')
+const addToCartBtn = document.getElementById('add-button')
+const shoppingList = document.getElementById('shoppingList')
+
+addToCartBtn.addEventListener('click', () => {
+  const inputValue = inputFieldEl.value
+
+  push(cartInDB, inputValue)
+
+  clearInputField()
+})
+  
+function clearInputField () {
+  inputFieldEl.value = ``
+}
+
+function displayShoppingList (itemValue, itemID) {
+
+  let newEl = document.createElement('li')
+  newEl.textContent = itemValue
+  newEl.id 
+  shoppingList.append(newEl)
+
+  newEl.addEventListener('click', () => {
+
+    let itemDBlocation = ref(database, `cart/${itemID}`)
+    remove(itemDBlocation)
+  })
+
+}
+
+onValue(cartInDB, function(snapshot) {
+
+  if (snapshot.exists()) {
+    shoppingList.innerHTML = "";
+  const shoppingListArray = Object.entries(snapshot.val())
+
+  shoppingListArray.forEach((item) => {
+    const itemID = item[0]
+    const itemValue = item [1]
+    
+    displayShoppingList(itemValue, itemID)
+  })
+  } else {
+    shoppingList.innerHTML = "No items here ... yet"
+  }
+
+  
+})
+
+
+
